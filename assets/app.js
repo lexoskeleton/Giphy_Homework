afff94ffa2c19dd2d950b5ca279e0c2e3e3135ed
lextiel <cummins.a88@gmail.com>
2018-06-23 17:38:05 -0400

    functions

--- assets/app.js
+++ assets/app.js
@@ -18,43 +18,43 @@ var topics = ['doug bradley',
             // Adding a class
             a.addClass("topic");
             // Adding a data-attribute with a value of the topic at index i
-            a.attr("data-name", topics[i]);
+            a.attr("data-topic", topics[i]);
             // Providing the button's text with a value of the topic at index i
             a.text(topics[i]);
             // Adding the button to the HTML
             $("#topics-view").append(a);
+            }
     }
-  }
 
-//grab gif data from giphy to populate gifs on button clicks with Ajax call
-//gifs should have ratings displayed
-$("button").on("click", function() {
-      var person = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+    //grab gif data from giphy to populate gifs on button clicks with Ajax call
+    //gifs should have ratings displayed
+    $(".topic").on("click", function() {
+        var person = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+             person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      $.ajax({
+        $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        .then(function(response) {
-          var results = response.data;
+        })
+         .then(function(response) {
+              var results = response.data;
 
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
+             for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='item'>");
 
-            var rating = results[i].rating;
+                    var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+                    var p = $("<p>").text("Rating: " + rating);
 
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
+                    var personImage = $("<img>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(personImage);
 
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
+                    $("#gifs-appear-here").prepend(gifDiv);
+                  }
         });
     });
 
@@ -72,7 +72,12 @@ $("button").on("click", function() {
         renderButtons();
       });
 
+      renderButtons();
+
     //create a function that pauses/unpauses gif animation
+
+    
+    
     //run all functions
 
 
